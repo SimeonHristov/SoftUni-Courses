9bbd7c0e711d5206ab8c84ddc7f1ecb5355be46b
shristov <simeonhr@gmail.com>
2021-02-03 10:52:09 +0200

    added Workshop demo

---
+++ Java Advanced Jan 2021/07 Workshop/Main.java
@@ -0,0 +1,11 @@
+package _13_Workshop;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+//        SmartArray smartArray = new SmartArray();
+//
+//        smartArray.add(13);
+    }
+}
