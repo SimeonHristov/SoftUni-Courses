4fb7fee81e9e3ac9a0053007f4667a6abb2ef027
shristov <simeonhr@gmail.com>
2021-02-09 10:03:03 +0200

    added P9 and P10 solution

---
+++ Java Advanced Jan 2021/09 Exercise - Generics/P10_Tuple/Tuple.java
@@ -0,0 +1,18 @@
+package _15_Generics_EXERCISE.P10_Tuple;
+
+public class Tuple <K,V> {
+    private K firstElement;
+    private V secondElement;
+
+    public Tuple(K firstElement, V secondElement) {
+        this.firstElement = firstElement;
+        this.secondElement = secondElement;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return String.format("%s -> %s", this.firstElement,this.secondElement);
+    }
+}
