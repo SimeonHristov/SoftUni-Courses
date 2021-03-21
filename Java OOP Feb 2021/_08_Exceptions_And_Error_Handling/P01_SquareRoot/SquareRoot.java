95e3f5b42ac934a068c92ffadf41365c9dbf5d65
shristov <simeonhr@gmail.com>
2021-03-21 17:40:04 +0200

    refactor P01_SquareRoot

--- Java OOP Feb 2021/_08_Exceptions_And_Error_Handling/P01_SquareRoot/SquareRoot.java
+++ Java OOP Feb 2021/_08_Exceptions_And_Error_Handling/P01_SquareRoot/SquareRoot.java
@@ -5,36 +5,21 @@ import java.util.Scanner;
 import java.util.function.Supplier;
 
 public class SquareRoot {
-    public static void main (String[] args) {
 
-        Scanner scanner = new Scanner (System.in);
+    public static void main(String[] args) {
 
-        testSQLException();
-
+        Scanner scan = new Scanner(System.in);
 
         try {
-            int n = Integer.parseInt (scanner.nextLine ());
-            System.out.println (Math.pow (n, 2.0));
-            return;// Finally ще се изпълни при всяко положение, но не и "hohoho"!!!
+            System.out.println(Math.sqrt(Integer.parseInt(scan.nextLine())));
 
-        } catch (NumberFormatException ex) {
-            System.out.println ("Invalid input " + ex.getMessage ());
-        } catch (Exception ex) {
-            ex.printStackTrace ();
+        } catch (NumberFormatException exception) {
+
+            System.out.println("Invalid input");
+
         } finally {
-            System.out.println ("Good bye");
+
+            System.out.println("Good bye");
         }
-        System.out.println ("hohoho");
-
-    }
-
-    private static
-    void testSQLException () {
-        try {
-            throw new SQLException();
-        } catch (SQLException sqlEx) {
-            throw new IllegalStateException("Cannot save invoice.", sqlEx);
-        }
-
     }
 }
