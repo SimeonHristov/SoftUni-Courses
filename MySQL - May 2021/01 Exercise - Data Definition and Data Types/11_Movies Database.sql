89e2944315118c0232219f7a7b73824d0b1e2a1d
shristov <simeonhr@gmail.com>
2021-05-27 21:56:10 +0300

    modified queries

--- MySQL - May 2021/01 Exercise - Data Definition and Data Types/11_Movies Database.sql
+++ MySQL - May 2021/01 Exercise - Data Definition and Data Types/11_Movies Database.sql
@@ -1,1 +1,66 @@
-qwe
+CREATE DATABASE `Movies`;
+
+use `movies`;
+
+CREATE TABLE `directors` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`director_name` VARCHAR (50) NOT NULL,
+`notes` TEXT
+);
+
+CREATE TABLE `genres` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`genre_name` VARCHAR (50) NOT NULL,
+`notes` TEXT
+);
+
+CREATE TABLE `categories` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`category_name` VARCHAR (50) NOT NULL,
+`notes` TEXT
+);
+
+CREATE TABLE `movies` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`title` VARCHAR (50) NOT NULL,
+`director_id` INT,
+`copyright_year` DATE,
+`lenght` TIMESTAMP,
+`genre_id` INT,
+`category_id` INT,
+`rating` FLOAT (2,1),
+`notes` TEXT
+);
+
+INSERT INTO `directors`
+VALUES
+(1, 'd1', NULL),
+(2, 'd2', NULL),
+(3, 'd3', NULL),
+(4, 'd4', NULL),
+(5, 'd5', NULL);
+
+INSERT INTO `genres`
+VALUES
+(1, 'g1', NULL),
+(2, 'g2', NULL),
+(3, 'g3', NULL),
+(4, 'g4', NULL),
+(5, 'g5', NULL);
+
+INSERT INTO `categories`
+VALUES
+(1, 'c1', NULL),
+(2, 'c2', NULL),
+(3, 'c3', NULL),
+(4, 'c4', NULL),
+(5, 'c5', NULL);
+
+INSERT INTO `movies`
+VALUES
+(1, 'm1', 5, '2020-12-01', NULL, 5, 1, 9.5, NULL),
+(2, 'm1', 4, '2020-12-01', NULL, 4, 2, 7.5, NULL),
+(3, 'm1', 3, '2020-12-01', NULL, 3, 3, 6.5, NULL),
+(4, 'm1', 2, '2020-12-01', NULL, 2, 4, 5.5, NULL),
+(5, 'm1', 1, '2020-12-01', NULL, 1, 5, 4.5, NULL);
+
--- MySQL - May 2021/01 Exercise - Data Definition and Data Types/12_Car Rental Database.sql
+++ MySQL - May 2021/01 Exercise - Data Definition and Data Types/12_Car Rental Database.sql
@@ -1,1 +1,84 @@
-qwe
+CREATE TABLE `categories` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`category` VARCHAR (50) NOT NULL,
+`daily_rate` FLOAT (5, 2),
+`weekly_rate` FLOAT (5, 2),
+`monthly_rate` FLOAT (5, 2),
+`weekend_rate` FLOAT (5,2)
+);
+
+CREATE TABLE `cars` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`plate_number` VARCHAR (10) NOT NULL,
+`make` VARCHAR (30) NOT NULL,
+`model` VARCHAR (30) NOT NULL,
+`car_year` DATE,
+`category_id` INT,
+`doors` INT,
+`picture` BLOB,
+`car_condition` TEXT,
+`available` BOOLEAN
+);
+
+CREATE TABLE `employees` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`first_name` VARCHAR (25) NOT NULL,
+`last_name` VARCHAR (25) NOT NULL,
+`title` CHAR (3),
+`notes` TEXT
+);
+
+CREATE TABLE `customers` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`driver_license_number` INT NOT NULL,
+`full_name` VARCHAR (25) NOT NULL,
+`address` VARCHAR (30),
+`city` VARCHAR (30),
+`zip_code` VARCHAR (10),
+`notes` TEXT
+);
+
+CREATE TABLE `rental_orders` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`employee_id` INT NOT NULL,
+`customer_id` INT NOT NULL,
+`car_id` INT NOT NULL,
+`car_condition` TEXT,
+`tank_level` INT,
+`kilometrage_start` INT,
+`kilometrage_end` INT,
+`total_kilometrage` INT,
+`start_date` DATE,
+`end_date` DATE,
+`total_days` INT,
+`rate_applied` FLOAT,
+`tax_rate` FLOAT,
+`order_status` VARCHAR (10),
+`notes` TEXT
+);
+
+
+INSERT INTO `categories` VALUES
+(1, 'c1', 2, 5, 25, 3),
+(2, 'c2', 2, 5, 25, 3),
+(3, 'c3', 2, 5, 25, 3);
+
+INSERT INTO `cars` VALUES
+(1, 'qwe', 'make', 'model', '2020-12-01', 1, 4, NULL, NULL, true),
+(2, 'qwe', 'make', 'model', '2020-12-01', 1, 4, NULL, NULL, true),
+(3, 'qwe', 'make', 'model', '2020-12-01', 1, 4, NULL, NULL, true);
+
+INSERT INTO `employees` VALUES
+(1, 'fn', 'ln', 'mr', NULL),
+(2, 'fn', 'ln', 'mr', NULL),
+(3, 'fn', 'ln', 'mr', NULL);
+
+INSERT INTO `customers` VALUES
+(1, 12341, 'fullname', 'address', 'city', 'zipcode', 'notes'),
+(2, 12341, 'fullname', 'address', 'city', 'zipcode', 'notes'),
+(3, 12341, 'fullname', 'address', 'city', 'zipcode', 'notes');
+
+INSERT INTO `rental_orders` VALUES
+(1, 1, 1, 1, 'good', 5, 123123, 123222, 99, '2020-12-01', '2020-12-02', 1, 4, 0.2, 'completed', NULL),
+(2, 1, 1, 1, 'good', 5, 123123, 123222, 99, '2020-12-01', '2020-12-02', 1, 4, 0.2, 'completed', NULL),
+(3, 1, 1, 1, 'good', 5, 123123, 123222, 99, '2020-12-01', '2020-12-02', 1, 4, 0.2, 'completed', NULL);
