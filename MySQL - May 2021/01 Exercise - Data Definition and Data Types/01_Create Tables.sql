317eb031ed3761f1ef0ee4ccfc95b18514ae57b5
shristov <simeonhr@gmail.com>
2021-05-27 11:25:34 +0300

    Data Definition and Data Types - Exercise - added some basic queries

---
+++ MySQL - May 2021/01 Exercise - Data Definition and Data Types/01_Create Tables.sql
@@ -0,0 +1,13 @@
+use `minions`;
+
+CREATE TABLE `minions` (
+`id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR (50) NOT NULL,
+`age` INT
+);
+
+CREATE TABLE `towns` (
+`town_id` INT PRIMARY KEY AUTO_INCREMENT,
+`name` VARCHAR (50) NOT NULL
+);
+
