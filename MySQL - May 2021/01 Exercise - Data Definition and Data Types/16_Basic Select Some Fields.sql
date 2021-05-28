c33d9e5bb8c3e44d6590aa7ab7d856d172b77ee3
shristov <simeonhr@gmail.com>
2021-05-28 11:03:28 +0300

    p 16,17,18

--- MySQL - May 2021/01 Exercise - Data Definition and Data Types/16_Basic Select Some Fields.sql
+++ MySQL - May 2021/01 Exercise - Data Definition and Data Types/16_Basic Select Some Fields.sql
@@ -1,1 +1,8 @@
-qwewq
+select `name` from `towns`
+order by `name` ASC;
+
+select `name` from `departments`
+order by `name` ASC;
+
+select `first_name`, `last_name`, `job_title`, `salary` from `employees`
+order by`salary` DESC;
--- MySQL - May 2021/01 Exercise - Data Definition and Data Types/17_Increase Employees Salary.sql
+++ MySQL - May 2021/01 Exercise - Data Definition and Data Types/17_Increase Employees Salary.sql
@@ -1,1 +1,4 @@
-qwe
+UPDATE  `employees`
+SET `salary` = (`salary` + (`salary`/ 10)) * 1.0
+WHERE `id` >= 1;
+
