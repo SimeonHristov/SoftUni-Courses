4bb9324dce13bfb08395cea112bbfd5bc2ec9782
shristov <simeonhr@gmail.com>
2021-06-08 09:47:41 +0300

    solution query

---
+++ MySQL - May 2021/05 Table Relations/Delete Mountains.sql
@@ -0,0 +1,14 @@
+CREATE TABLE mountains(
+	id int primary key auto_increment not null,
+	name VARCHAR(30) not null
+);
+
+CREATE TABLE peaks(
+	id int primary key auto_increment not null,
+	name VARCHAR(30) not null,
+    mountain_id int,
+	CONSTRAINT fk_mountain_id 
+    foreign key (mountain_id)
+    REFERENCES mountains(id)
+  	on delete cascade
+);
