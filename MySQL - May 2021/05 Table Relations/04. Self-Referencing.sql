ce4a32024f17f448097154ad29ece57024030e82
shristov <simeonhr@gmail.com>
2021-06-10 19:47:29 +0300

    self referencing query

---
+++ MySQL - May 2021/05 Table Relations/04. Self-Referencing.sql
@@ -0,0 +1,21 @@
+create table teachers (
+teacher_id int primary key auto_increment,
+name varchar (30) not null,
+manager_id int
+);
+
+alter table teachers auto_increment = 101;
+
+insert into teachers (name, manager_id)
+values
+('John', NULL),
+('Maya',106),
+('Silvia',106),
+('Ted',105),
+('Mark',101),
+('Greta',101);
+
+alter table teachers
+add constraint fk_teachers_managers
+foreign key (manager_id)
+references teachers(teacher_id);
