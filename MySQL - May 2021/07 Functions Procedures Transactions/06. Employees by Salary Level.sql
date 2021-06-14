2338ef3c44aa6515a763ad8613efd5dd6c0dc361
shristov <simeonhr@gmail.com>
2021-06-14 15:30:34 +0300

    solution query

---
+++ MySQL - May 2021/07 Functions Procedures Transactions/06. Employees by Salary Level.sql
@@ -0,0 +1,19 @@
+delimiter ##
+CREATE PROCEDURE `usp_get_employees_by_salary_level` (level_salary varchar(10))
+BEGIN
+	select first_name, last_name from employees
+	where 
+    (case 
+    when level_salary = 'low' then 
+    salary BETWEEN 0 and 29999 
+    
+    when level_salary = 'average' then   
+    salary BETWEEN 30000 and 50000 
+ 
+    when level_salary = 'high' then 
+    salary > 50000
+	END)
+    order by first_name desc, last_name desc; 
+END##
+
+
