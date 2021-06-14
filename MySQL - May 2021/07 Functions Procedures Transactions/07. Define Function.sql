a83b5c9b7b83fd3678f9130c934c28503b5bf712
shristov <simeonhr@gmail.com>
2021-06-14 15:37:46 +0300

    solution query with regex

---
+++ MySQL - May 2021/07 Functions Procedures Transactions/07. Define Function.sql
@@ -0,0 +1,17 @@
+delimiter ##
+
+CREATE FUNCTION ufn_is_word_comprised(set_of_letters varchar(50), word varchar(50))
+RETURNS BIT
+DETERMINISTIC
+BEGIN
+
+RETURN word REGEXP (concat('^[', set_of_letters, ']+$'));
+END ##
+
+
+SELECT ufn_is_word_comprised('oistmiahf', 'Sofia');
+SELECT ufn_is_word_comprised('oistmiahf', 'halves');
+SELECT ufn_is_word_comprised('bobr', 'Rob');
+SELECT ufn_is_word_comprised('pppp', 'Guy');
+
+DROP FUNCTION IF EXISTS ufn_is_word_comprised;
