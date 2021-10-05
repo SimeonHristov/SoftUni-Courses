9d775d5a59c5ee11bc4615cfba980d33ce22e159
shristov <simeonhr@gmail.com>
2021-10-05 20:43:20 +0300

    lab project initial commit

---
+++ Spring Fundamentals - Sep 2021/mobile_v2/src/main/java/com/example/mobile_v2/MobileV2Application.java
@@ -0,0 +1,13 @@
+package com.example.mobile_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MobileV2Application.class, args);
+    }
+
+}
