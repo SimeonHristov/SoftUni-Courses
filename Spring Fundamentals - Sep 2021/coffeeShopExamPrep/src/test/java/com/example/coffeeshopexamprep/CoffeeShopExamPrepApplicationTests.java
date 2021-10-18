24e4600483af61603ec1d38ee6b0e635c72ad094
shristov <simeonhr@gmail.com>
2021-10-18 20:32:14 +0300

    CoffeeShopExamPrep (setup + static resources + html templates + application.properties)

---
+++ Spring Fundamentals - Sep 2021/coffeeShopExamPrep/src/test/java/com/example/coffeeshopexamprep/CoffeeShopExamPrepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeeshopexamprep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeShopExamPrepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
