7d320e4746e6c4ef4dffe3080620c81a5426a4fa
shristov <simeonhr@gmail.com>
2022-09-28 14:49:42 +0300

    ServerServiceImpl - overriding methods

--- server/src/main/java/com/example/server/service/impl/ServerServiceImpl.java
+++ server/src/main/java/com/example/server/service/impl/ServerServiceImpl.java
@@ -1,14 +1,21 @@
 package com.example.server.service.impl;
 
+import com.example.server.enumeration.Status;
 import com.example.server.model.Server;
 import com.example.server.repository.ServerRepository;
 import com.example.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Collection;
+
+import static com.example.server.enumeration.Status.SERVER_DOWN;
+import static com.example.server.enumeration.Status.SERVER_UP;
 
 @RequiredArgsConstructor
 @Service
@@ -20,29 +27,37 @@ public class ServerServiceImpl implements ServerService {
 
     @Override
     public Server create(Server server) {
-        log.info("Saving  new server: {}", server.getName());
+        log.info("Saving new server: {}", server.getName());
         server.setImageUrl(setServerImageUrl());
         return serverRepository.save(server);
     }
 
     @Override
-    public Server ping(String ipAddress) {
-        return null;
+    public Server ping(String ipAddress) throws IOException {
+        log.info("Pinging server IP: {}", ipAddress);
+        Server server = serverRepository.findByIpAddress(ipAddress);
+        InetAddress address = InetAddress.getByName(ipAddress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepository.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> list(int limit) {
-        return null;
+        log.info("Fetching all servers");
+        return serverRepository.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server get(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepository.findById(id).get();
     }
 
     @Override
     public Server update(Server server) {
-        return null;
+        log.info("Updating server: {}", server.getName());
+        return serverRepository.save(server);
     }
 
     @Override
