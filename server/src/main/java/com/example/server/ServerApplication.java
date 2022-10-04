917fff5fd765815e52560faa0fe9c1e30a21854a
shristov <simeonhr@gmail.com>
2022-10-04 09:05:47 +0300

    added Response fix extended ServerResource getServerImage add SQL config

--- server/src/main/java/com/example/server/ServerApplication.java
+++ server/src/main/java/com/example/server/ServerApplication.java
@@ -1,7 +1,12 @@
 package com.example.server;
 
+import com.example.server.enumeration.Status;
+import com.example.server.model.Server;
+import com.example.server.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
@@ -9,5 +14,24 @@ public class ServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository) {
+		return args -> {
+			serverRepository.save(new Server(null,"192.168.1.160",
+					"Prod","64 GB", "Production Environment Server",
+					"http://localhost:8080/server/image/s1.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null,"192.168.1.58",
+					"QA","64 GB", "QA Environment Server",
+					"http://localhost:8080/server/image/s2.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null,"192.168.1.21",
+					"Test","32 GB", "Test Environment Server",
+					"http://localhost:8080/server/image/s2.png", Status.SERVER_UP));
+			serverRepository.save(new Server(null,"192.168.1.14",
+					"UAT","32 GB", "UAT Environment Server",
+					"http://localhost:8080/server/image/s3.png", Status.SERVER_UP));
+		};
+	}
+
 
 }
--- server/src/main/java/com/example/server/model/Response.java
+++ server/src/main/java/com/example/server/model/Response.java
@@ -1,16 +1,24 @@
 package com.example.server.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Map;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
+
+@Data
+@SuperBuilder
+@JsonInclude(NON_NULL) //if something us NULL, don't show to the user
 public class Response {
     protected LocalDateTime timeStamp;
     protected int statusCode;
     protected HttpStatus status;
-    protected String reason;
+    protected String reason; //will be NULL, won't send to front
     protected String message;
-    protected String developerMessage;
+    protected String developerMessage; //will be NULL, won't send to front
     protected Map<?, ?> data;
 }
