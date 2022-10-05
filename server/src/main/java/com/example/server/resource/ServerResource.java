34ef60c4a12061884e54772cba87ccbf87833aa6
shristov <simeonhr@gmail.com>
2022-10-05 10:37:55 +0300

    getServerImage (minor fix)

--- server/src/main/java/com/example/server/resource/ServerResource.java
+++ server/src/main/java/com/example/server/resource/ServerResource.java
@@ -95,6 +95,6 @@ public class ServerResource {
 
     @GetMapping(value = "/image/{fileName}", produces = IMAGE_PNG_VALUE)
     public byte[] getServerImage (@PathVariable ("fileName") String fileName) throws IOException {
-        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "Downloads" + fileName));
+        return Files.readAllBytes(Paths.get(System.getProperty("user.home") + "/Downloads/" + fileName));
     }
 }
